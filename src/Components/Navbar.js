911def0cb89f86edcba7f6683074d21097523528
progdaiben <daibenoatmeal3@gmail.com>
2023-12-27 13:02:50 +0800

    updating links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,12 +19,12 @@ class Navbar extends Component {
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                <a class="nav-link active" href="/#">
+                <a class="nav-link active" href="/react-test/#/">
                   Home
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="/#/about">
+                <a class="nav-link" href="/react-test/#/about">
                   About
                 </a>
               </li>
